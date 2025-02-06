46fed77a0c83738b515c89d21a5017124888d045
Simon Sorg <simon.sorg@student.hpi.de>
2025-02-06 17:11:43 +0000

    fix: set explicit path

--- tests/test_full.py
+++ tests/test_full.py
@@ -1,10 +1,11 @@
 from lean_repl_py import LeanREPLHandler, LeanREPLProofState
 import pytest
+from pathlib import Path
 
 
 @pytest.fixture
 def handler():
-    yield LeanREPLHandler()
+    yield LeanREPLHandler(Path(__file__).parent.parent / "repl")
 
 
 def test_send_command(handler):
